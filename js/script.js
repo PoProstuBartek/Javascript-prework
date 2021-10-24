adda8e4d6fe3e504d41bdc3fd209347ec2d93ba0
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2021-10-24 10:46:17 +0200

    Add playerMove script

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+
+// Losowanie ruchu komputera
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -21,3 +23,19 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// Wybór ruchu gracza 
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+  printMessage('Błędny ruch!  Wybraliśmy za Ciebie :)');
+}
+printMessage('Twój ruch: ' + playerMove);
